3f3c1d4ae07482d7a26f3e5b7bf35b01c1e93bda
kairavb <kairavb8@gmail.com>
2025-03-11 15:12:30 +0530

    counter

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Food from './Food.jsx'
 import Button from './Button.jsx'
+import Counter from './Counter.jsx'
 
 function App() {
   return (
     <>
       <Header/>
+      <Counter></Counter>
       <Food/>
       <Button></Button>
       <Footer/>
